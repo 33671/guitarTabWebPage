7015b38d848447ffe6f0bc93371cbc1578fce419
zhang <1434926171@qq.com>
2022-07-22 18:04:05 +0800

    登陆页逻辑

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,6 +1,5 @@
 <template>
   <body>
-    <!-- <canvas> </canvas> -->
     <div class="login-register">
       <div class="contain row">
         <div class="small-box col-4" :class="{ active: isLogin }">
@@ -15,16 +14,20 @@
             <button class="sbutton" @click="changeType">登录</button>
           </div>
         </div>
-        <div class="big-box col-8" :class="{ active: isLogin }">
+        <div
+          class="big-box col-8"
+          :class="{ active: isLogin }"
+          style="background: #eceef6"
+        >
           <div class="big-contain" v-if="isLogin">
             <div class="btitle">账户登录</div>
             <div class="bform">
-              <input type="email" placeholder="邮箱" v-model="form.useremail" />
+              <input type="email" placeholder="邮箱" v-model="form.name" />
               <!-- <span class="errTips" v-if="emailError">* 邮箱填写错误 *</span> -->
               <input
                 type="password"
                 placeholder="密码"
-                v-model="form.userpwd"
+                v-model="form.password"
               />
               <div
                 class="row flex items-center justify-between"
@@ -41,21 +44,21 @@
               <!-- <span class="errTips" v-if="emailError">* 密码填写错误 *</span> -->
             </div>
 
-            <button class="bbutton" @click="reglog">登录</button>
+            <button class="bbutton" @click="userlogin">登录</button>
           </div>
           <div class="big-contain" v-else>
             <div class="btitle">创建账户</div>
             <div class="bform">
-              <input type="text" placeholder="用户名" v-model="form.username" />
+              <input type="text" placeholder="用户名" v-model="form.name" />
               <span class="errTips" v-if="existed">* 用户名已经存在！ *</span>
               <input type="email" placeholder="邮箱" v-model="form.useremail" />
               <input
                 type="password"
                 placeholder="密码"
-                v-model="form.userpwd"
+                v-model="form.password"
               />
               <div
-                class="row flex items-center justify-between"
+                class="row flex items-center justify-between no-wrap"
                 style="width: 50%"
               >
                 <input
@@ -67,12 +70,13 @@
                 <img style="max-width: 120px" src="/api/captcha/" alt="" />
               </div>
             </div>
-            <button class="bbutton" @click="reglog">注册</button>
+            <button class="bbutton" @click="userreg">注册</button>
           </div>
         </div>
       </div>
       <!-- </v-card> -->
     </div>
+    <canvas> </canvas>
   </body>
 </template>
 
@@ -80,253 +84,254 @@
 import { onMounted, ref } from "vue";
 import { axios } from "boot/axios";
 import { useQuasar } from "quasar";
-
 const $q = useQuasar();
 //dialog插件，提示用户登录成功与否
-function reglog() {
-  $q.dialog({
-    title: `<img src="/api/captcha/" alt="" />`,
-    message: "Some message",
-    html: true,
-  })
-    .onOk(() => {
-      // console.log('OK')
-    })
-    .onCancel(() => {
-      // console.log('Cancel')
-    })
-    .onDismiss(() => {
-      // console.log('I am triggered on both OK and Cancel')
-    });
+
+async function userlogin() {
+  var loginform = new FormData();
+  loginform.append("name", form.value.name);
+  loginform.append("password", form.value.password);
+  loginform.append("captcha", "111");
+  const aa = await axios.post("/api/login", loginform);
+  console.log(aa);
+  // $q.dialog({
+  //   title: `<img src="/api/captcha/" alt="" />`,
+  //   message: "Some message",
+  //   html: true,
+  // })
+  //   .onOk(() => {
+  //     // console.log('OK')
+  //   })
+  //   .onCancel(() => {
+  //     // console.log('Cancel')
+  //   })
+  //   .onDismiss(() => {
+  //     // console.log('I am triggered on both OK and Cancel')
+  //   });
 }
-onMounted(() => {});
-function login() {
-  getCaptcha();
+async function userreg() {
+  var regform = new FormData();
+  regform.append("name", form.value.name);
+  regform.append("email", form.value.useremail);
+  regform.append("password", form.value.password);
+  regform.append("captcha", "111");
+  const aa = await axios.post("/api/register/", regform);
+  console.log(aa);
 }
+
 const dialog3 = ref(false);
 const isLogin = ref(false);
 const emailError = ref(false);
 const passwordError = ref(false);
 const existed = ref(false);
 const form = ref({
-  username: "",
+  name: "",
   useremail: "",
-  userpwd: "",
+  password: "",
+  captcha: "",
 });
 function changeType() {
   isLogin.value = !isLogin.value;
-  form.value.username = "";
+  form.value.name = "";
   form.value.useremail = "";
-  form.value.userpwd = "";
-}
-async function getCaptcha() {
-  const captcha = await axios.get("/api/captcha/");
-  console.log(captcha.data);
-}
-async function toDataURL(url, callback) {
-  var reader = new FileReader();
-  return;
-  reader.onloadend = function () {
-    callback(reader.result);
-  };
-  reader.readAsDataURL(xhr.response);
+  form.value.password = "";
 }
 
-// mounted() {
-//   const STAR_COLOR = "#000000";
-//   const STAR_SIZE = 3;
-//   const STAR_MIN_SCALE = 0.2;
-//   const OVERFLOW_THRESHOLD = 50;
-//   const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8;
+onMounted(() => {
+  const STAR_COLOR = "#000000";
+  const STAR_SIZE = 3;
+  const STAR_MIN_SCALE = 0.2;
+  const OVERFLOW_THRESHOLD = 50;
+  const STAR_COUNT = (window.innerWidth + window.innerHeight) / 8;
 
-//   const canvas = document.querySelector("canvas"),
-//     context = canvas.getContext("2d");
+  const canvas = document.querySelector("canvas"),
+    context = canvas.getContext("2d");
 
-//   let scale = 1, // device pixel ratio
-//     width,
-//     height;
+  let scale = 1, // device pixel ratio
+    width,
+    height;
 
-//   let stars = [];
+  let stars = [];
 
-//   let pointerX, pointerY;
+  let pointerX, pointerY;
 
-//   let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+  let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
 
-//   let touchInput = false;
+  let touchInput = false;
 
-//   generate();
-//   resize();
-//   step();
+  generate();
+  resize();
+  step();
 
-//   window.onresize = resize;
-//   canvas.onmousemove = onMouseMove;
-//   canvas.onmouseleave = onMouseLeave;
-//   canvas.ontouchmove = onTouchMove;
-//   canvas.ontouchend = onMouseLeave;
+  window.onresize = resize;
+  canvas.onmousemove = onMouseMove;
+  canvas.onmouseleave = onMouseLeave;
+  canvas.ontouchmove = onTouchMove;
+  canvas.ontouchend = onMouseLeave;
 
-//   function generate() {
-//     for (let i = 0; i < STAR_COUNT; i++) {
-//       stars.push({
-//         x: 0,
-//         y: 0,
-//         z: STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE),
-//       });
-//     }
-//   }
+  function generate() {
+    for (let i = 0; i < STAR_COUNT; i++) {
+      stars.push({
+        x: 0,
+        y: 0,
+        z: STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE),
+      });
+    }
+  }
 
-//   function placeStar(star) {
-//     star.x = Math.random() * width;
-//     star.y = Math.random() * height;
-//   }
+  function placeStar(star) {
+    star.x = Math.random() * width;
+    star.y = Math.random() * height;
+  }
 
-//   function recycleStar(star) {
-//     let direction = "z";
+  function recycleStar(star) {
+    let direction = "z";
 
-//     let vx = Math.abs(velocity.x),
-//       vy = Math.abs(velocity.y);
+    let vx = Math.abs(velocity.x),
+      vy = Math.abs(velocity.y);
 
-//     if (vx > 1 || vy > 1) {
-//       let axis;
+    if (vx > 1 || vy > 1) {
+      let axis;
 
-//       if (vx > vy) {
-//         axis = Math.random() < vx / (vx + vy) ? "h" : "v";
-//       } else {
-//         axis = Math.random() < vy / (vx + vy) ? "v" : "h";
-//       }
+      if (vx > vy) {
+        axis = Math.random() < vx / (vx + vy) ? "h" : "v";
+      } else {
+        axis = Math.random() < vy / (vx + vy) ? "v" : "h";
+      }
 
-//       if (axis === "h") {
-//         direction = velocity.x > 0 ? "l" : "r";
-//       } else {
-//         direction = velocity.y > 0 ? "t" : "b";
-//       }
-//     }
+      if (axis === "h") {
+        direction = velocity.x > 0 ? "l" : "r";
+      } else {
+        direction = velocity.y > 0 ? "t" : "b";
+      }
+    }
 
-//     star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
+    star.z = STAR_MIN_SCALE + Math.random() * (1 - STAR_MIN_SCALE);
 
-//     if (direction === "z") {
-//       star.z = 0.1;
-//       star.x = Math.random() * width;
-//       star.y = Math.random() * height;
-//     } else if (direction === "l") {
-//       star.x = -OVERFLOW_THRESHOLD;
-//       star.y = height * Math.random();
-//     } else if (direction === "r") {
-//       star.x = width + OVERFLOW_THRESHOLD;
-//       star.y = height * Math.random();
-//     } else if (direction === "t") {
-//       star.x = width * Math.random();
-//       star.y = -OVERFLOW_THRESHOLD;
-//     } else if (direction === "b") {
-//       star.x = width * Math.random();
-//       star.y = height + OVERFLOW_THRESHOLD;
-//     }
-//   }
+    if (direction === "z") {
+      star.z = 0.1;
+      star.x = Math.random() * width;
+      star.y = Math.random() * height;
+    } else if (direction === "l") {
+      star.x = -OVERFLOW_THRESHOLD;
+      star.y = height * Math.random();
+    } else if (direction === "r") {
+      star.x = width + OVERFLOW_THRESHOLD;
+      star.y = height * Math.random();
+    } else if (direction === "t") {
+      star.x = width * Math.random();
+      star.y = -OVERFLOW_THRESHOLD;
+    } else if (direction === "b") {
+      star.x = width * Math.random();
+      star.y = height + OVERFLOW_THRESHOLD;
+    }
+  }
 
-//   function resize() {
-//     scale = window.devicePixelRatio || 1;
+  function resize() {
+    scale = window.devicePixelRatio || 1;
 
-//     width = window.innerWidth * scale;
-//     height = window.innerHeight * scale;
+    width = window.innerWidth * scale;
+    height = window.innerHeight * scale;
 
-//     canvas.width = width;
-//     canvas.height = height;
+    canvas.width = width;
+    canvas.height = height;
 
-//     stars.forEach(placeStar);
-//   }
+    stars.forEach(placeStar);
+  }
 
-//   function step() {
-//     context.clearRect(0, 0, width, height);
+  function step() {
+    context.clearRect(0, 0, width, height);
 
-//     update();
-//     render();
+    update();
+    render();
 
-//     requestAnimationFrame(step);
-//   }
+    requestAnimationFrame(step);
+  }
 
-//   function update() {
-//     velocity.tx *= 0.96;
-//     velocity.ty *= 0.96;
+  function update() {
+    velocity.tx *= 0.96;
+    velocity.ty *= 0.96;
 
-//     velocity.x += (velocity.tx - velocity.x) * 0.8;
-//     velocity.y += (velocity.ty - velocity.y) * 0.8;
+    velocity.x += (velocity.tx - velocity.x) * 0.8;
+    velocity.y += (velocity.ty - velocity.y) * 0.8;
 
-//     stars.forEach((star) => {
-//       star.x += velocity.x * star.z;
-//       star.y += velocity.y * star.z;
+    stars.forEach((star) => {
+      star.x += velocity.x * star.z;
+      star.y += velocity.y * star.z;
 
-//       star.x += (star.x - width / 2) * velocity.z * star.z;
-//       star.y += (star.y - height / 2) * velocity.z * star.z;
-//       star.z += velocity.z;
+      star.x += (star.x - width / 2) * velocity.z * star.z;
+      star.y += (star.y - height / 2) * velocity.z * star.z;
+      star.z += velocity.z;
 
-//       // recycle when out of bounds
-//       if (
-//         star.x < -OVERFLOW_THRESHOLD ||
-//         star.x > width + OVERFLOW_THRESHOLD ||
-//         star.y < -OVERFLOW_THRESHOLD ||
-//         star.y > height + OVERFLOW_THRESHOLD
-//       ) {
-//         recycleStar(star);
-//       }
-//     });
-//   }
+      // recycle when out of bounds
+      if (
+        star.x < -OVERFLOW_THRESHOLD ||
+        star.x > width + OVERFLOW_THRESHOLD ||
+        star.y < -OVERFLOW_THRESHOLD ||
+        star.y > height + OVERFLOW_THRESHOLD
+      ) {
+        recycleStar(star);
+      }
+    });
+  }
 
-//   function render() {
-//     stars.forEach((star) => {
-//       context.beginPath();
-//       context.lineCap = "round";
-//       context.lineWidth = STAR_SIZE * star.z * scale;
-//       context.globalAlpha = 0.5 + 0.5 * Math.random();
-//       context.strokeStyle = STAR_COLOR;
+  function render() {
+    stars.forEach((star) => {
+      context.beginPath();
+      context.lineCap = "round";
+      context.lineWidth = STAR_SIZE * star.z * scale;
+      context.globalAlpha = 0.5 + 0.5 * Math.random();
+      context.strokeStyle = STAR_COLOR;
 
-//       context.beginPath();
-//       context.moveTo(star.x, star.y);
+      context.beginPath();
+      context.moveTo(star.x, star.y);
 
-//       var tailX = velocity.x * 2,
-//         tailY = velocity.y * 2;
+      var tailX = velocity.x * 2,
+        tailY = velocity.y * 2;
 
-//       // stroke() wont work on an invisible line
-//       if (Math.abs(tailX) < 0.1) tailX = 0.5;
-//       if (Math.abs(tailY) < 0.1) tailY = 0.5;
+      // stroke() wont work on an invisible line
+      if (Math.abs(tailX) < 0.1) tailX = 0.5;
+      if (Math.abs(tailY) < 0.1) tailY = 0.5;
 
-//       context.lineTo(star.x + tailX, star.y + tailY);
+      context.lineTo(star.x + tailX, star.y + tailY);
 
-//       context.stroke();
-//     });
-//   }
+      context.stroke();
+    });
+  }
 
-//   function movePointer(x, y) {
-//     if (typeof pointerX === "number" && typeof pointerY === "number") {
-//       let ox = x - pointerX,
-//         oy = y - pointerY;
+  function movePointer(x, y) {
+    if (typeof pointerX === "number" && typeof pointerY === "number") {
+      let ox = x - pointerX,
+        oy = y - pointerY;
 
-//       velocity.tx = velocity.tx + (ox / 8) * scale * (touchInput ? 1 : -1);
-//       velocity.ty = velocity.ty + (oy / 8) * scale * (touchInput ? 1 : -1);
-//     }
+      velocity.tx = velocity.tx + (ox / 8) * scale * (touchInput ? 1 : -1);
+      velocity.ty = velocity.ty + (oy / 8) * scale * (touchInput ? 1 : -1);
+    }
 
-//     pointerX = x;
-//     pointerY = y;
-//   }
+    pointerX = x;
+    pointerY = y;
+  }
 
-//   function onMouseMove(event) {
-//     touchInput = false;
+  function onMouseMove(event) {
+    touchInput = false;
 
-//     movePointer(event.clientX, event.clientY);
-//   }
+    movePointer(event.clientX, event.clientY);
+  }
 
-//   function onTouchMove(event) {
-//     touchInput = true;
+  function onTouchMove(event) {
+    touchInput = true;
 
-//     movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
+    movePointer(event.touches[0].clientX, event.touches[0].clientY, true);
 
-//     event.preventDefault();
-//   }
+    event.preventDefault();
+  }
 
-//   function onMouseLeave() {
-//     console.log("1");
-//     pointerX = null;
-//     pointerY = null;
-//   }
-// },
+  function onMouseLeave() {
+    console.log("1");
+    pointerX = null;
+    pointerY = null;
+  }
+});
 </script>
 <style>
 body {
@@ -361,7 +366,9 @@ a:hover {
   color: #fff;
 }
 .login-register {
-  width: 70vw;
+  width: 50vw;
+  z-index: 1;
+  opacity: 0.9;
 }
 .contain {
   width: 100%;
