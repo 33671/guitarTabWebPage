27af7bf8d7897886ded15fbe26c95e2d2b5a3337
Referi <765511198@qq.com>
2024-04-12 02:41:25 +0800

    （せつなくて いとおしい） （令人揪心又令人心爱）  今ならば 分かる気がする 若是如今 好像已经明白  （しあわせで くるおしい） 幸福却又让人心乱神迷  あの日泣けなかった僕を 照耀着无法哭泣的我

--- quasar.config.js
+++ quasar.config.js
@@ -78,7 +78,7 @@ module.exports = configure(function (ctx) {
         type: "http",
       },
       port: 8080,
-      open: true, // opens browser window automatically
+      open: false, // opens browser window automatically
       proxy: {
         "/api": {
           target: "http://localhost:5000/",
